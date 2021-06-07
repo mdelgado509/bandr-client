8e3caf9f941ac0dec0bf8754714727775b089d68
mdelgado509 <mdelgado509@gmail.com>
2021-06-07 01:53:32 -0400

    Add deployed backend url to production in config file

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://desolate-fjord-95980.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
